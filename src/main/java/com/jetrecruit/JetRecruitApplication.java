9f8ee9f04be9a7673c4e259204178a61ed6a5b54
DESKTOP-IEPVL3O\Admin <deepanshu.verma@adjecti.com>
2023-04-25 18:59:14 +0530

    initial commit

---
+++ src/main/java/com/jetrecruit/JetRecruitApplication.java
@@ -0,0 +1,13 @@
+package com.jetrecruit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JetRecruitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JetRecruitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jetrecruit/JetRecruitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jetrecruit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JetRecruitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
